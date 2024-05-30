5b1b7abd90c05aad4fe1c0881c2639f729439f00
Éric GILLES <eric.gilles@etu.umontpellier.fr>
2024-05-30 17:27:46 +0200

    Add FireBase Storage & Add Modify/Add Offer Page

--- JobFindMe/app/build.gradle.kts
+++ JobFindMe/app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- JobFindMe/app/src/main/java/com/example/jobfindme/ui/screens/JobForm.kt
+++ JobFindMe/app/src/main/java/com/example/jobfindme/ui/screens/JobForm.kt
@@ -1,46 +1,28 @@
 package com.example.jobfindme.ui.screens
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.jobfindme.R
 import com.example.jobfindme.ui.components.BottomNav
-import com.example.jobfindme.ui.components.CrossedCirclesShapeBlue
-import com.example.jobfindme.ui.components.LogoutButton
+import com.example.jobfindme.ui.components.ModifyOffer
+import com.example.jobfindme.ui.components.NewOffer
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun FormJob(modifier: Modifier = Modifier, navController: NavController, firestore: FirebaseFirestore, firebaseAuth: FirebaseAuth) {
+fun FormJob(modifier: Modifier = Modifier, navController: NavController, firestore: FirebaseFirestore, firebaseAuth: FirebaseAuth, addOffer : Boolean) {
   Scaffold(
     bottomBar = {
       BottomNav(navController= navController)
     }
   ){
-    CrossedCirclesShapeBlue()
-
+    if (addOffer){
+      NewOffer(navController= navController, firebaseAuth = firebaseAuth, firestore = firestore)
+    }else {
+      ModifyOffer(navController= navController, firebaseAuth = firebaseAuth, firestore = firestore)
+    }
   }
 }
